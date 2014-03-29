0ce355441da79f94bd1b69966ad19792ccfad45c
Ronny Archilinux <rapidtyping@hotmail.com>
2014-03-29 18:00:50 -0500

    cap8( parte 2 ) - RequestContext, como elemento importante a la hora de cargar nuestros archivos estáticos, aunque se puede evitar su uso definiendo un 'template_name'

--- inicio/urls.py
+++ inicio/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, include, url
 from .views import index, index2
 
 urlpatterns = patterns('',
-    url(r'^$' , 'inicio.views.index'),
+    url(r'^$' , index.as_view() ),
     url(r'^index/$' , index2.as_view() ),
 )
--- inicio/views.py
+++ inicio/views.py
@@ -1,8 +1,29 @@
+# -*- encoding:utf-8 -*-
 from django.shortcuts import render_to_response
 from django.views.generic import TemplateView
+#cap8 parte2
+from django.template import RequestContext    
+#cap8 parte2 - Con esto ya carga los archivos estáticos    
+#class index(TemplateView):
+#    template_name = 'inicio/index.html'
 
-def index(request):
-    return render_to_response('inicio/index.html')
+#cap8 parte2 - Otra forma de renderizar PERO FALTA
+#              un parámetro para que cargue los estáticos
+#class index(TemplateView):
+
+#    def get(self, request, *args, **kwargs):
+#        return render_to_response('inicio/index.html')
+
+#cap8 parte2 - Explicación: Al no usar TemplateView se debe
+#               agregar el RequestContext(request)
+class index(TemplateView):
+
+    def get(self, request, *args, **kwargs):
+        return render_to_response('inicio/index.html',
+                        context_instance = RequestContext(request))
+
+
+
     
 class index2(TemplateView):
     template_name = 'inicio/index2.html'
